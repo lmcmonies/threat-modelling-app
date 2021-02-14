c608baa03a6c3b5bf64f0d6b6b8cc3a2375aff12
lmcmonies <lmcmonies@live.co.uk>
2021-02-14 21:29:38 +0000

    not working

---
+++ client/src/composables/getSubCollection.js
@@ -0,0 +1,50 @@
+import { ref, watchEffect } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
+const getSubCollection = (collection, documentId, subCollection) => {
+    const documents = ref(null)
+    const error = ref(null)
+
+    let collectionRef = projectFirestore.collection(collection).doc(documentId).collection(subCollection)
+    
+    // onSnapshot() - real time listener for DB
+    //A document is added, changed, deleted
+    //then user gets sent a snapshot.
+    //Contains all docs and data at that moment in time
+    collectionRef.onSnapshot((snap) => {
+        console.log('snapshot')
+        let results = []
+        snap.docs.forEach(doc => {
+
+            //doc.data().createdAt - required, otherwise user recieves 
+            //local version of snap using local createdAt. Meaning, 
+            // user recieves not fully created snapshot. 
+            results.push({...doc.data(), id:doc.id})
+          console.log(doc.data())
+          for (let i=0; i<results.length; i++){
+            console.log("EMAIL: " + results[i].id)
+        }
+          
+        })
+       
+
+        documents.value = results
+
+        error.value = null
+    },  (err) => {
+        console.log(err.message)
+        documents.value = null
+        error.value = 'could not fetch data'
+    })
+
+    //executes when component it's being used in unmounts
+    //unsub from prev collection when watcher is stopped
+    // watchEffect((onInvalidate) => {
+    //   onInvalidate(() => unsub())
+    // })
+
+    return { documents, error }
+    
+}
+
+export default getSubCollection
